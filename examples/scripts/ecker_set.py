3e1f8e4d32ddb43c84eb3002e8b0af2857d2bcc9
Valentin Sulzer <vsulzer@umich.edu>
2020-02-13 10:46:31 -0500

    #729 events fix

--- examples/scripts/ecker_set.py
+++ examples/scripts/ecker_set.py
@@ -1,9 +1,10 @@
 import pybamm as pb
+import numpy as np
 
-pb.set_logging_level("DEBUG")
+pb.set_logging_level("INFO")
 
 model = pb.lithium_ion.DFN()
-# model.convert_to_format = "python"
+model.convert_to_format = "python"
 
 chemistry = pb.parameter_sets.Ecker2015
 # chemistry = pb.parameter_sets.Marquis2019
@@ -11,6 +12,7 @@ parameter_values = pb.ParameterValues(chemistry=chemistry)
 
 sim = pb.Simulation(model, parameter_values=parameter_values)
 
-solver = pb.CasadiSolver(mode="safe")
-sim.solve(solver=solver)
-
+solver = pb.IDAKLUSolver()  # mode="fast")
+t_eval = np.linspace(0, 1, 100)
+sim.solve(t_eval=t_eval, solver=solver)
+sim.plot()
