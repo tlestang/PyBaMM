4bbbaf22f2f8e6ce0edff27584f4f563a2887df5
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-01-08 15:36:17 +0000

    Use TimeDFN class

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,26 +3,27 @@
 
 import pybamm as pb
 
-def prep_model():
-    model = pb.lithium_ion.SPM()
-    geometry = model.default_geometry
+class TimeDFN():
+    def setup(self):
+        model = pb.lithium_ion.DFN()
+        geometry = model.default_geometry
 
-    # load parameter values and process model and geometry
-    param = model.default_parameter_values
-    param.process_model(model)
-    param.process_geometry(geometry)
+        # load parameter values and process model and geometry
+        param = model.default_parameter_values
+        param.process_model(model)
+        param.process_geometry(geometry)
 
-    # set mesh
-    mesh = pb.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
+        # set mesh
+        mesh = pb.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
 
-    # discretise model
-    disc = pb.Discretisation(mesh, model.default_spatial_methods)
-    disc.process_model(model)
+        # discretise model
+        disc = pb.Discretisation(mesh, model.default_spatial_methods)
+        disc.process_model(model)
 
-    return model
+        self.model = model
+        self.solver = model.default_solver()
 
-def time_SPM(model):
-    solver = pb.ScipySolver()
-    solver.solve(model, [0, 3600])
-time_SPM.setup_cache = prep_model
+    def time_solve(self):
+        self.solver.solve(self.model, [0, 3600])
+
     
