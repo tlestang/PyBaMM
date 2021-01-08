a3e260117f8797252de5f3b323ba2cef1a34a9df
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-01-08 11:39:50 +0000

    Add benchmark for SPM

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,36 +1,28 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import pybamm as pb
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+def prep_model():
+    model = pb.lithium_ion.SPM()
+    geometry = model.default_geometry
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
+    # load parameter values and process model and geometry
+    param = model.default_parameter_values
+    param.process_model(model)
+    param.process_geometry(geometry)
 
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
+    # set mesh
+    mesh = pb.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
 
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
+    # discretise model
+    disc = pb.Discretisation(mesh, model.default_spatial_methods)
+    disc.process_model(model)
 
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+    return model
 
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+def time_SPM(model):
+    solver = pb.ScipySolver()
+    solver.solve(model, [0, 3600])
+time_SPM.setup_cache = prep_model
+    
